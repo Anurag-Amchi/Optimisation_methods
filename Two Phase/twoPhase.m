2ae3e04d65c66ddfe2bcb65164d80903715689cd
Anurag Amchi <112432737+Anurag-Amchi@users.noreply.github.com>
2025-03-11 13:25:15 +0530

    added redundancy

--- Two Phase/twoPhase.m
+++ Two Phase/twoPhase.m
@@ -51,7 +51,7 @@ fprintf('\nInitial Simplex table:\n');
 disp(table);
 % disp(C1);
 
-% Good luck starting phase-2 without ts
+% Good luck starting phase without ts
 coeffIdx=n+1:n+lessThan;
 
 for i=1:size(C1,2)
@@ -113,6 +113,43 @@ while any(Zj_Cj<0)
     disp(Zj_Cj);
 end
 
+if any(coeffIdx>n+lessThan+greaterThan)
+    fprintf('Redundancy occurs\n');
+end
+while any(coeffIdx>n+lessThan+greaterThan)
+    greaterValidx=find(coeffIdx>n+lessThan+greaterThan);
+    i=greaterValidx(1);
+    if table(i,end)>0
+        fprintf('Inconsistent LPP\n');
+        return;
+    else
+        for j=1:n+greaterThan+lessThan
+            if any(coeffIdx==j)
+                continue;
+            else
+                table(i,:)=table(i,:)/table(i,j);
+                for k=1:m
+                    if k~=rowIdx
+                        table(k,:)=table(k,:)-table(i,:)*(table(k,j));
+                    end
+                end
+                coeffIdx(i)=j;
+                c1=[];
+                for k=1:m
+                    c1=[c1,C1(coeffIdx(k))];
+                end
+                A=table(:,1:end-1);
+                Zj_Cj=c1*A-C1;
+                fprintf('Simplex table after iteration %d\n',itr);
+                disp(table);
+                fprintf('Zj-Cj:\n')
+                disp(Zj_Cj);
+                break;
+            end
+        end
+    end
+    fprintf('here\n')
+end
 fprintf('\nPhase 2\n');
 % ez extraction as we structured our input
 table2=table(:,1:end-greaterThan-equalTo-1);
@@ -139,14 +176,14 @@ while any(Zj_Cj<0)
     pivotCol=A(:,colIdx);
     if all(pivotCol<=0)
         fprintf('Unbounded Solution\n');
-        break;
+        return;
     end
     xb=table2(:,size(table2,2));
     ratios= xb ./ (pivotCol + (pivotCol == 0)*eps);
     % disp(ratios);
     if all(ratios<0)
         fprintf('Unbounded Solution\n');
-        break;
+        return;
     end
     ratMin=1e6;
     rowIdx=1;
